ed90e13c7600a477c71fd5e0a04b3729334c7499
Antoha5001 <plantsvzzombi@mail.ru>
2018-04-01 20:24:26 +0700

    123

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.less1:82/admin/');
+define('HTTP_CATALOG', 'http://opencart.less1:82/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.less1:82/admin/');
+define('HTTPS_CATALOG', 'http://opencart.less1:82/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer2/OSPanel/domains/opencart.less1/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer2/OSPanel/domains/opencart.less1/system/');
+define('DIR_IMAGE', 'C:/OpenServer2/OSPanel/domains/opencart.less1/image/');
+define('DIR_STORAGE', 'C:/OpenServer2/OSPanel/domains/storage/');
+define('DIR_CATALOG', 'C:/OpenServer2/OSPanel/domains/opencart.less1/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart_one');
+define('DB_PORT', '3307');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
